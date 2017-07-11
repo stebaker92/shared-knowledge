b381fe75f7a24e2c16f304d9b1bbb5edfca0aacb
Steve Baker <stebaker92@users.noreply.github.com>
2017-07-11 15:07:46 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -18,7 +18,7 @@ function npp($file) { & "C:\Program Files (x86)\Notepad++\notepad++.exe" $file}
 
 #SQL
 function deploy-motormart() {
-	cd E:\MotorMart\Carfinance.MotorMart\
+	cd E:\MotorMart\Carfinance.MotorMart.Schema\
 	msbuild /p:BuildProjectReferences=false .\Carfinance.Motormart.Schema\Carfinance.Motormart.Schema.sqlproj
 	& "C:\Program Files (x86)\Microsoft SQL Server\130\DAC\bin\SqlPackage.exe" /Action:Publish `
 	/SourceFile:Carfinance.MotorMart.Schema/bin/debug/Carfinance.MotorMart.Schema.dacpac `
